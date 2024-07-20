50e05e96ae7b40d1224a960fa1d8bc48e2c484b5
Waltersyd <sydney.onyango@student.moringaschool.com>
2024-07-20 23:32:20 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+//function to find matching drivers considering names
+function findMatching(drivers, query){
+    return drivers.filter(driver => driver.toLowerCase() === query.toLowerCase());
+}
+
+
+//Function to fuzzy matching drivers based on names
+function fuzzyMatch(drivers, query){
+    return drivers.filter(drivers => drivers.toLowerCase().startsWith(query.toLowerCase()));
+}
+
+
+//Function to match drivers considering the name propery
+function matchName(drivers, query){
+    return drivers.filter(driver => driver.name.toLowerCase().startsWith(query.toLowerCase()));
+
+}
